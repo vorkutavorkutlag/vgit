016be8287a7ecffdfe4f4badb08729da044419e3
vorkuta <vorkutavorkutlag@gmail.com>
2026-07-29 00:33:25 +0200

    woopsie daisy

--- main.cpp
+++ main.cpp
@@ -3,93 +3,34 @@
 
 #include <nlohmann/json.hpp>
 
-namespace fs = std::filesystem;
+#include "CLI11.hpp"
 
-constexpr const char* s_init{"init"};
-constexpr const char* s_branch{"branch"};
-constexpr const char* s_switch{"switch"};
-constexpr const char* s_add{"add"};
-constexpr const char* s_rm{"rm"};
-constexpr const char* s_active_branch{"active_branch"};
-constexpr const char* s_delete_flag1{"-d"};
-constexpr const char* s_delete_flag2{"-D"};
+namespace fs = std::filesystem;
 
 constexpr const char* empty_json_list{"[]"};
 constexpr const char* empty_json_dict{"{}"};
 
+constexpr const char* s_active_branch{"active_branch"};
+
 const fs::path ROOT_PATH{".vgit"};
-const fs::path STAGE_PATH = ROOT_PATH / "active_stage.json";
-const fs::path BRANCHES_PATH = ROOT_PATH / "branches";
-const fs::path INFO_PATH = ROOT_PATH / "active_info.json";
+const fs::path BRANCHES_PATH{ROOT_PATH / "branches"};
+const fs::path INFO_PATH{ROOT_PATH / "active_info.json"};
+const fs::path STAGE_PATH_P{"active_stage.json"};
 constexpr __mode_t ROOT_PERMS{0777U};
 
-enum class ERROR {
-    NOT_INITED,
-    CANT_INIT,
-    CANT_ACCESS,
-    NO_BRANCH,
-    CANT_CREATE,
-    NO_SUICIDE,
-    COUNT
+enum class Status { Success, Warning, Error };
+
+struct Result {
+    Status status;
+    std::string_view message;
 };
 
-enum class WARNING { NO_EFFECT, COUNT };
-
-enum class SUCCESS {
-    INITED,
-    STAGE_ADD,
-    STAGE_RM,
-    BRANCH,
-    DELETE,
-    SWITCH,
-    COUNT
-};
-
-enum class INFO { VGIT, SWITCH, COUNT };
-
-static constexpr std::array<std::string_view, static_cast<size_t>(ERROR::COUNT)>
-    ERROR_MESSAGES{
-        "vgit repository is uninitialized.\n run `vgit init` to initialize.",
-
-        "vgit could not initialize repository here.\n check permissions, make "
-        "sure there is no existing repository.",
-        "Could not access item.",
-        "No active branch selected. To create, vgit branch <name>",
-        "Could not create new.",
-        "Switch branches before deleting this branch."};
-
-static constexpr std::array<std::string_view,
-                            static_cast<size_t>(WARNING::COUNT)>
-    WARNING_MESSAGES{"Action had no effect"};
-
-static constexpr std::array<std::string_view,
-                            static_cast<size_t>(SUCCESS::COUNT)>
-    SUCCESS_MESSAGES{"vgit empty repository successfully initialized.",
-                     "File successfully added to staging area.",
-                     "File successfully removed from staging area.",
-                     "Successfully created branch.",
-                     "Successfully deleted.",
-                     "Successfully switched."};
-
-static constexpr std::array<std::string_view, static_cast<size_t>(INFO::COUNT)>
-    INFO_MESSAGES{"vgit - barebones git",
-                  "Proper use: vgit switch <branch-name>"};
-
-/* The finals are killers. One evil, one good.*/
-
-void final(ERROR e) {
-    std::cout << ERROR_MESSAGES[static_cast<size_t>(e)] << std::endl;
-    exit(static_cast<int>(e));
+[[noreturn]] inline void finally(Result res) {
+    std::cout << res.message << std::endl;
+    exit(static_cast<int>(res.status));
 }
 
-void final(SUCCESS s) {
-    std::cout << SUCCESS_MESSAGES[static_cast<size_t>(s)] << std::endl;
-    exit(0);
-}
-
-/*---------------------------------------------------------------------------*/
-
-constexpr nlohmann::json get_info() {
+nlohmann::json get_active_info() {
     if (!fs::exists(INFO_PATH)) {
         std::ofstream out{INFO_PATH};
         out << empty_json_dict;
@@ -104,313 +45,121 @@ constexpr nlohmann::json get_info() {
     return loaded;
 }
 
-constexpr nlohmann::json get_staging() {
-    if (!fs::exists(STAGE_PATH)) {
-        std::ofstream out{STAGE_PATH};
-        out << empty_json_list;
-        out.close();
-    }
-
-    std::ifstream in{STAGE_PATH};
-    nlohmann::json loaded;
-    in >> loaded;
-    in.close();
-
-    return loaded;
-}
-
-constexpr void save_staging(const nlohmann::json& json) {
-    std::ofstream out{STAGE_PATH, std::ios::trunc};
-    out << json;
-    out.close();
-}
-
-constexpr void save_info(const nlohmann::json& json) {
+void set_active_info(const nlohmann::json& json) {
     std::ofstream out{INFO_PATH, std::ios::trunc};
     out << json;
     out.close();
 }
 
-// returns a random 20 byte hash
-constexpr std::string get_random_hash() {
-    std::string h;
-    for (size_t i{0uz}; i < 20; ++i) h.push_back(rand() % 16);
-    return h;
-}
-
-/*---------------------------------------------------------------------------*/
-
-/* Checks if directory '.vgit' exists in CWD and is accessible */
+/* ------------------------------ */
 inline bool is_inited() { return fs::is_directory(ROOT_PATH); }
 
-bool has_branch() {
-    auto json = get_info();
-    auto it = json.find(s_active_branch);
-    return it != json.end() && fs::is_directory(*it);
-}
-
-std::string get_branch() {
-    auto json = get_info();
+std::string get_active_branch() {
+    auto json = get_active_info();
     auto it = json.find(s_active_branch);
     if (it == json.end()) return std::string{};
     return fs::path(*it).filename();
 }
 
-void set_branch(const std::string& name) {
-    auto json = get_info();
-    json[s_active_branch] = name;
-    save_info(json);
+Result set_active_branch(const std::string& b_name) {
+    if (!fs::is_directory(BRANCHES_PATH / b_name))
+        return {Status::Error, "Branch does not exist."};
+    auto json = get_active_info();
+    json[s_active_branch] = b_name;
+    set_active_info(json);
+    return {Status::Success, "Successfully switched branch."};
 }
 
-/* Initializes an empty repository at this directory */
-int init() {
-    return !is_inited() && !mkdir(ROOT_PATH.c_str(), ROOT_PERMS) &&
-           !mkdir(BRANCHES_PATH.c_str(), ROOT_PERMS);
+inline auto get_branches() { return fs::directory_iterator(BRANCHES_PATH); }
+
+Result handle_init() {
+    if (!is_inited() && !mkdir(ROOT_PATH.c_str(), ROOT_PERMS) &&
+        !mkdir(BRANCHES_PATH.c_str(), ROOT_PERMS))
+        return {Status::Success, "Successfully initialized empty repository."};
+    return {Status::Error, "Could not initialize new empty repository here."};
 }
 
-/*---------------------------------------------------------------------------*/
+Result handle_branch(const std::string& b_name) {
+    fs::path would_be{BRANCHES_PATH / b_name};
+    if (fs::is_directory(would_be))
+        return {Status::Error, "Branch by same name already exists."};
+    if (mkdir(would_be.c_str(), ROOT_PERMS))
+        return {Status::Error, "Could not create new branch."};
+    set_active_branch(b_name);
+    return {Status::Success, "Created new branch & switched."};
+}
 
-int add(const char* filename) {
-    std::fstream f{filename};
-    if (!f.good()) return 0;
-    f.close();
+Result handle_branch() {
+    static std::string all_branches;
+    const std::string active_branch = get_active_branch();
 
-    auto json = get_staging();
+    if (active_branch.empty()) return {Status::Success, all_branches};
 
-    // check if already added
-    for (const auto& item : json) {
-        if (!strcmp(item.get<std::string>().c_str(), filename)) return -1;
+    for (const auto& b : get_branches()) {
+        const std::string b_name = b.path().filename().generic_string();
+        if (active_branch == b_name) all_branches += " + ";
+        (all_branches += b_name) += "\n";
     }
 
-    json.push_back(filename);
+    all_branches.pop_back();
 
-    save_staging(json);
-
-    return 1;
+    return {Status::Success, all_branches};
 }
 
-int rm(const char* filename) {
-    std::fstream f{filename};
-    if (!f.good()) return 0;
-    f.close();
-
-    auto json = get_staging();
-
-    for (auto it{json.begin()}; it != json.end(); ++it) {
-        if (!strcmp(it->get<std::string>().c_str(), filename)) {
-            json.erase(it);
-            save_staging(json);
-            return 1;
-        }
-    }
-
-    return -1;
+Result handle_switch(const std::string& b_name) {
+    return set_active_branch(b_name);
 }
 
-// only displays active branches and current branch
-int branch() {
-    fs::directory_iterator dir{BRANCHES_PATH};
-    const auto active = get_branch();
-    for (const auto& br : dir) {
-        const auto bname = br.path().filename().generic_string();
-        std::cout << (active == bname ? "+ " + bname : bname) << std::endl;
-    }
-    return 0;
+Result handle_nuke() {
+    std::cout << "Are you sure you want to delete the current "
+                 "repository?\n[y/N]\n";
+    std::string input;
+    std::getline(std::cin, input);
+    if (input != "y" && input != "Y") return {Status::Success, "Aborted nuke."};
+
+    if (fs::remove_all(ROOT_PATH)) return {Status::Success, "BOOM!"};
+    return {Status::Error, "Failed to remove the repository."};
 }
 
-int branch(const char* name) {
-    fs::path alleged_branch{BRANCHES_PATH};
-    alleged_branch /= name;
-    if (fs::is_directory(alleged_branch)) return 0;
-    if (mkdir(alleged_branch.c_str(), ROOT_PERMS)) return 0;
-    set_branch(name);
-    return 1;
-};
-
-int d_branch(const char* name) {
-    fs::path alleged_branch{BRANCHES_PATH};
-    alleged_branch /= name;
-    if (!fs::is_directory(alleged_branch)) return 0;
-    if (!alleged_branch.filename().compare(get_branch())) return -1;
-
-    return fs::remove_all(alleged_branch);
-}
-
-int v_switch(const char* name) {
-    fs::path alleged_branch{BRANCHES_PATH};
-    alleged_branch /= name;
-    if (!fs::is_directory(alleged_branch)) return 0;
-    if (name == get_branch()) return -1;
-    set_branch(name);
-    return 1;
-}
-
-// pushes a new commit to the commit stack
-int commit(const char* message) {
-    const std::string h{get_random_hash()};
-    const std::string m{message};
-    const auto json = get_staging();
-    return 0;
-}
-
-auto add_resolver(int r) {
-    switch (r) {
-        case 0:
-            return ERROR_MESSAGES[static_cast<size_t>(ERROR::CANT_ACCESS)];
-        case -1:
-            return WARNING_MESSAGES[static_cast<size_t>(WARNING::NO_EFFECT)];
-        case 1:
-            return SUCCESS_MESSAGES[static_cast<size_t>(SUCCESS::STAGE_ADD)];
-        default:
-            return std::string_view{};
-    }
-}
-
-auto rm_resolver(int r) {
-    switch (r) {
-        case 0:
-            return ERROR_MESSAGES[static_cast<size_t>(ERROR::CANT_ACCESS)];
-        case -1:
-            return WARNING_MESSAGES[static_cast<size_t>(WARNING::NO_EFFECT)];
-        case 1:
-            return SUCCESS_MESSAGES[static_cast<size_t>(SUCCESS::STAGE_RM)];
-        default:
-            return std::string_view{};
-    }
-}
-
-auto branch_resolver(int r) {
-    switch (r) {
-        case 0:
-            return ERROR_MESSAGES[static_cast<size_t>(ERROR::CANT_CREATE)];
-        case 1:
-            return SUCCESS_MESSAGES[static_cast<size_t>(SUCCESS::BRANCH)];
-        default:
-            return std::string_view{};
-    }
-}
-
-auto d_branch_resolver(int r) {
-    switch (r) {
-        case 0:
-            return ERROR_MESSAGES[static_cast<size_t>(ERROR::CANT_ACCESS)];
-        case -1:
-            return ERROR_MESSAGES[static_cast<size_t>(ERROR::NO_SUICIDE)];
-        case 1:
-            return SUCCESS_MESSAGES[static_cast<size_t>(SUCCESS::DELETE)];
-        default:
-            return std::string_view{};
-    }
-}
-
-auto switch_resolver(int r) {
-    // funny
-    switch (r) {
-        case 0:
-            return ERROR_MESSAGES[static_cast<size_t>(ERROR::CANT_ACCESS)];
-        case 1:
-            return SUCCESS_MESSAGES[static_cast<size_t>(SUCCESS::SWITCH)];
-        case -1:
-            return WARNING_MESSAGES[static_cast<size_t>(
-                WARNING::NO_EFFECT)];  // nothing changed
-        default:
-            return std::string_view{};
-    }
-}
-
-/*
-Hi. In the first variation, there will only be support for vgit:
-- add
-- remove
-- commit -m "..."
-- push
-*/
+/* ------------------------------ */
 
 int main(int argc, char* argv[]) {
-    srand(std::chrono::duration_cast<std::chrono::seconds>(
-              std::chrono::system_clock::now().time_since_epoch())
-              .count());
+    CLI::App app{"vgit - the barebones local git"};
 
-    if (argc < 2) return 0;  // should print description
+    auto* init = app.add_subcommand("init", "Initialize repository");
 
-    /* --------- INIT ---------*/
+    std::string branch_name;
+    auto* branch = app.add_subcommand("branch", "Manage branches");
+    branch->add_option("name", branch_name, "Branch name");
 
-    if (!strcmp(argv[1], s_init)) {
-        if (init())
-            final(SUCCESS::INITED);
-        else
-            final(ERROR::CANT_INIT);
+    std::string switch_name;
+    auto* swtch = app.add_subcommand("switch", "Switch branch");
+    swtch->add_option("name", switch_name, "Branch name");
+
+    std::vector<std::string> add_files;
+    auto* add = app.add_subcommand("add", "Add files to stage");
+    add->add_option("file", add_files, "File to add");
+
+    auto* nuke =
+        app.add_subcommand("nuke", "Delete repository in working directory");
+
+    CLI11_PARSE(app, argc, argv);
+
+    if (*init) finally(handle_init());
+
+    if (!is_inited())
+        finally(
+            {Status::Error, "Initialize repository before further action."});
+
+    if (*nuke) finally(handle_nuke());
+
+    if (*branch) {
+        if (branch_name.empty()) finally(handle_branch());
+        finally(handle_branch(branch_name));
     }
 
-    if (!is_inited()) {
-        final(ERROR::NOT_INITED);
-    }
+    if (*swtch) finally(handle_switch(switch_name));
 
-    /*  allow things like status and whatever here.
-        anything that impacts the staging area is forbidden. */
-
-    /* --------- BRANCH ---------*/
-
-    if (!strcmp(argv[1], s_branch)) {
-        if (argc == 2) return branch();
-
-        bool delete_mode = false;
-
-        for (int i{2}; i < argc; ++i) {
-            if (!strcmp(argv[i], s_delete_flag1) ||
-                !strcmp(argv[i], s_delete_flag2))
-                delete_mode = ++i;
-
-            if (!delete_mode)
-                std::cout << "Branch '" << argv[i]
-                          << "': " << branch_resolver(branch(argv[i]))
-                          << std::endl;
-            else
-                std::cout << "Branch '" << argv[i]
-                          << "': " << d_branch_resolver(d_branch(argv[i]))
-                          << std::endl;
-        }
-
-        return 0;
-    }
-
-    /* --------- SWITCH ---------*/
-    if (!strcmp(argv[1], s_switch)) {
-        if (argc > 2)
-            std::cout << argv[2] << ": " << switch_resolver(v_switch(argv[2]))
-                      << std::endl;
-        else
-            std::cout << INFO_MESSAGES[static_cast<size_t>(INFO::SWITCH)]
-                      << std::endl;
-
-        return 0;
-    }
-
-    if (!has_branch()) {
-        final(ERROR::NO_BRANCH);
-    }
-
-    /* --------- ADD --------- */
-
-    if (!strcmp(argv[1], s_add)) {
-        for (int i{2}; i < argc; ++i)
-            std::cout << argv[i] << ": " << add_resolver(add(argv[i]))
-                      << std::endl;
-
-        return 0;
-    }
-
-    /* --------- REMOVE --------- */
-
-    if (!strcmp(argv[1], s_rm)) {
-        for (int i{2}; i < argc; ++i)
-            std::cout << argv[i] << ": " << rm_resolver(rm(argv[i]))
-                      << std::endl;
-
-        return 0;
-    }
-
-    else {
-        // should warn about unrecognized command
-    }
+    if (get_active_branch().empty())
+        finally({Status::Error, "Select branch before further action."});
 }
